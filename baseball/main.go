4879f100fec0cd3346082578170e91f746c4a0c0
DukHyung Kim <idealismboy@gmail.com>
2022-05-19 00:36:20 +0900

    move to baseball dir

---
+++ baseball/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+const length = 4
+
+func main() {
+	randNum := makeRandomNumber()
+	fmt.Println("정답:", randNum)
+	fmt.Println()
+
+	fmt.Print("기회: ")
+	var chance int
+	_, err := fmt.Scan(&chance)
+	if err != nil {
+		log.Println("숫자가 아님")
+		return
+	}
+
+	for i := 0; i < chance; i++ {
+		fmt.Print("숫자 입력: ")
+		var num string
+		_, err = fmt.Scan(&num)
+		if err != nil {
+			log.Println("입력이 제대로 되지 않았음")
+			return
+		}
+
+		_, err = strconv.ParseInt(num, 10, 64)
+		if err != nil {
+			log.Println("숫자가 아님")
+			return
+		}
+
+		if len(num) != length {
+			log.Printf("%d 자리 숫자 입력해야됨\n", length)
+			continue
+		}
+
+		var (
+			strike = 0
+			ball   = 0
+		)
+
+		for m := range num {
+			for n := range randNum {
+				if num[m] == randNum[n] && m == n {
+					strike++
+					break
+				}
+
+				if num[m] == randNum[n] {
+					ball++
+					break
+				}
+			}
+		}
+
+		if strike == 0 && ball == 0 {
+			fmt.Println("out")
+			continue
+		} else {
+			fmt.Printf("strike: %d, ball: %d\n", strike, ball)
+		}
+
+		if strike == len(randNum) {
+			fmt.Println("you win!")
+			break
+		}
+	}
+}
+
+func makeRandomNumber() string {
+	var nums = "0123456789"
+	var randNumBuilder strings.Builder
+	for i := 0; i < length; i++ {
+		idx := rand.Intn(len(nums))
+		randNumBuilder.WriteRune(rune(nums[idx]))
+		nums = nums[:idx] + nums[idx+1:]
+	}
+	randNum := randNumBuilder.String()
+	return randNum
+}
